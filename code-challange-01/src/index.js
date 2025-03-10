7cf10577fd122dbece49ce2594b99b8ce12edf30
Patrick Wolf <patrick@Patricks-MacBook-Pro.local>
2025-03-10 20:51:32 +0100

    change username

--- code-challange-01/src/index.js
+++ code-challange-01/src/index.js
@@ -75,7 +75,7 @@ function Skill({ skillName, color, level }) {
   // if (level === "beginner") {
   //   emoji = "😺";
   // } else if (level === "intermediate") {
-  //   emoji = "🤓";
+  //   emoji = "🤓-";
   // } else if (level === "advanced") {
   //   emoji = "💪";
   // }
